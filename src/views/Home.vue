a31550304c6c8ef3cd304bf2fd474585d821c7f8
liuyihsuan <liuyihsuan1012@gmail.com>
2021-10-18 20:29:53 +0800

    add home time icon part

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -52,7 +52,10 @@
           <div class="container-project-footer">
             <div class="cpf-line"></div>
             <div class="time-money-part">
-              <span class="time"><i class="far fa-clock"></i>剩下4小時</span>
+              <span class="time"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clock" viewBox="0 0 16 16">
+  <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z"/>
+  <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z"/>
+</svg>剩下4小時</span>
               <div class="money">NT$123,558</div>
             </div>
           </div>
@@ -75,7 +78,10 @@
           <div class="container-project-footer">
             <div class="cpf-line"></div>
             <div class="time-money-part">
-              <span class="time"><i class="far fa-clock"></i>剩下4小時</span>
+              <span class="time"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clock" viewBox="0 0 16 16">
+  <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z"/>
+  <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z"/>
+</svg>剩下4小時</span>
               <div class="money">NT$123,558</div>
             </div>
           </div>
@@ -104,7 +110,10 @@
           <div class="container-project-footer">
             <div class="cpf-line"></div>
             <div class="time-money-part">
-              <span class="time"><i class="far fa-clock"></i>剩下4小時</span>
+              <span class="time"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clock" viewBox="0 0 16 16">
+  <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z"/>
+  <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z"/>
+</svg>剩下4小時</span>
               <div class="money">NT$123,558</div>
             </div>
           </div>
