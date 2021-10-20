8ec5f39d06215b4085094d4f9ae3fe923e9c0a76
liuyihsuan <liuyihsuan1012@gmail.com>
2021-10-21 01:20:19 +0800

    modified homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -52,6 +52,36 @@
       </div>
     </nav>
   </div>
+  <!-- 挺好店專區 -->
+  <div class="covid">
+    <div class="covid-img">
+      <img src="https://i.imgur.com/HnDfduT.png" class="img-fluid" alt="..." />
+    </div>
+    <div class="covid-title">
+      <div class="covid-svg">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="35"
+          height="35"
+          fill="currentColor"
+          class="bi bi-shield-fill-plus"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M8 0c-.69 0-1.843.265-2.928.56-1.11.3-2.229.655-2.887.87a1.54 1.54 0 0 0-1.044 1.262c-.596 4.477.787 7.795 2.465 9.99a11.777 11.777 0 0 0 2.517 2.453c.386.273.744.482 1.048.625.28.132.581.24.829.24s.548-.108.829-.24a7.159 7.159 0 0 0 1.048-.625 11.775 11.775 0 0 0 2.517-2.453c1.678-2.195 3.061-5.513 2.465-9.99a1.541 1.541 0 0 0-1.044-1.263 62.467 62.467 0 0 0-2.887-.87C9.843.266 8.69 0 8 0zm-.5 5a.5.5 0 0 1 1 0v1.5H10a.5.5 0 0 1 0 1H8.5V9a.5.5 0 0 1-1 0V7.5H6a.5.5 0 0 1 0-1h1.5V5z"
+          />
+        </svg>
+      </div>
+      <h3>挺好店應援計畫</h3>
+      <span class="covid-span">
+        疫情讓許多你我的愛店面臨經營困難，嘖嘖規劃近期啟動「挺好店應援計畫」，提供實體空間／店家經營者和需要實體空間做展演的團隊應變疫情的上架方案。<a
+          href=""
+          >瞭解更多</a
+        >
+      </span>
+    </div>
+  </div>
   <!-- img -->
   <!-- 需新增遮罩效果 -->
   <div class="container">
@@ -151,7 +181,188 @@
           <div class="container-project-footer">
             <div class="cpf-line"></div>
             <div class="time-money-part">
-              <!-- 時間如何靠左 再回來修改 -->
+              <span class="time"
+                ><svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-clock"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z"
+                  />
+                  <path
+                    d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z"
+                  /></svg
+                >剩下4小時</span
+              >
+              <div class="money">NT$123,558</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-4">
+        <div class="card text-start" style="width: 16rem">
+          <div class="card-top">
+            <a href="">
+              <img
+                src="https://i.imgur.com/WV4ViWO.jpg"
+                class="card-img-top"
+                alt=""
+              />
+              <h5 class="card-title">流光百色 2022日曆</h5>
+            </a>
+          </div>
+          <div class="card-body pb4">
+            <span class="">設計 By <a class="" href="">不只是</a></span>
+            <p class="card-text">讓心成為三稜鏡，日光穿透，虹彩斑斕閃爍</p>
+          </div>
+          <div class="container-project-footer">
+            <div class="cpf-line"></div>
+            <div class="time-money-part">
+              <span class="time"
+                ><svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-clock"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z"
+                  />
+                  <path
+                    d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z"
+                  /></svg
+                >剩下4小時</span
+              >
+              <div class="money">NT$123,558</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-4">
+        <div class="card text-start" style="width: 16rem">
+          <div class="card-top">
+            <a href="">
+              <img
+                src="https://i.imgur.com/ELfBBRy.jpg"
+                class="card-img-top"
+                alt=""
+              />
+              <h5 class="card-title">
+                厭世好療癒桌曆｜35組紙娃娃穿搭療癒你的厭世日常
+              </h5>
+            </a>
+          </div>
+          <div class="card-body">
+            <span class="card-body-span"
+              >插畫漫畫 By <a class="" href="">厭世女子文創工作室</a></span
+            >
+            <p class="card-text">
+              現代社會讓人生活壓力大，厭世成為大家的共通點，《厭世女子》希望透過插畫圖文道出你心中的厭世，【厭世好療癒套組】因此...
+            </p>
+          </div>
+          <div class="container-project-footer">
+            <div class="cpf-line"></div>
+            <div class="time-money-part">
+              <span class="time"
+                ><svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-clock"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z"
+                  />
+                  <path
+                    d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z"
+                  /></svg
+                >剩下4小時</span
+              >
+              <div class="money">NT$123,558</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <nav aria-label="Page navigation example">
+    <ul class="pagination justify-content-center">
+      <li class="page-item disabled">
+        <a class="page-link" href="#" tabindex="-1" aria-disabled="true"
+          >上一頁</a
+        >
+      </li>
+      <li class="page-item"><a class="page-link" href="#">1</a></li>
+      <li class="page-item"><a class="page-link" href="#">2</a></li>
+      <li class="page-item"><a class="page-link" href="#">3</a></li>
+      <li class="page-item">
+        <a class="page-link" href="#">下一頁</a>
+      </li>
+    </ul>
+  </nav>
+  <div class="line"></div>
+  <div class="container project-navbar">
+    <!-- 商品方案分類 及按鈕 -->
+    <div class="row mt-4 mb-4 justify-content-between">
+      <div class="col-4">
+        <h4>預購式專案</h4>
+      </div>
+      <div class="col-4">
+        <button type="button" class="btn btn-outline-primary">
+          更多
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-caret-right-fill"
+            viewBox="0 0 16 16"
+          >
+            <path
+              d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"
+            />
+          </svg>
+        </button>
+      </div>
+    </div>
+  </div>
+  <div class="container home">
+    <!-- 商品區 -->
+    <div class="row">
+      <div class="col-4">
+        <div class="card text-start" style="width: 16rem">
+          <div class="card-top">
+            <a href="">
+              <img
+                src="https://i.imgur.com/2P79MF2.png"
+                class="card-img-top"
+                alt=""
+              />
+              <h5 class="card-title">
+                防災教育品牌《 火柴貓大作戰 | 火災求生桌遊》
+              </h5>
+            </a>
+          </div>
+          <div class="card-body pb4">
+            <span class=""
+              >遊戲 By <a class="" href="">dosomething studio</a></span
+            >
+            <p class="card-text">
+              火柴貓大作戰是由曾經獲得許多國際設計獎項的團隊dosomething
+              studio，研發設計的火災求生桌遊，希望...
+            </p>
+          </div>
+          <div class="container-project-footer">
+            <div class="cpf-line"></div>
+            <div class="time-money-part">
               <span class="time"
                 ><svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -359,6 +570,39 @@
 .nav-link {
   border-left: 1px solid #dcdcdc;
 }
+// 挺好店
+.covid {
+  position: relative;
+  .covid-img {
+    img {
+      width: 100%;
+      height: auto;
+    }
+  }
+  .covid-title {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    max-width: 26rem;
+    .covid-svg {
+      // 有問題 這個要RWD 必須找到置中方式 
+      margin-left: 11rem;
+      position: relative;
+      width: 4rem;
+      height: 4rem;
+      background-color: #fff;
+      border-radius: 50%;
+      svg {
+        position: absolute;
+        color: #0a6b0a;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+      }
+    }
+  }
+}
 // img
 .img-part {
   position: relative;
@@ -429,22 +673,50 @@
     }
   }
 }
+// empty line
+// .cpf-line {
+//   width: 100%;
+//   height: 0.15rem;
+//   background-color: var(--line-border-empty);
+//   border-radius: 0.2rem;
+//   margin-top: 0.7rem;
+// }
+/* 注意下fill設定 */
+.cpf-line {
+  width: 100%;
+  height: 0.15rem;
+  background-color: var(--line-border-fill);
+  border-radius: 0.2rem;
+}
+
+.time-money-part {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  .time {
+    line-height: 1.8rem;
+    font-size: 1rem;
+    svg {
+      margin-bottom: 0.2rem;
+    }
+  }
+}
 
 .pagination {
   margin-top: 2rem;
 }
 .line {
-  width:100%;
-  height:0.01rem;
+  width: 100%;
+  height: 0.01rem;
   background-color: #dcdcdc;
 }
 .footer {
   height: 18rem;
-.footer-part a{
-  text-decoration: none;
-  color: #0252a1;
-  line-height: 2rem;
-}
+  .footer-part a {
+    text-decoration: none;
+    color: #0252a1;
+    line-height: 2rem;
+  }
 }
 </style>
 <script>
