e07392b2bcca85eb5bfe951899c32c0f6449abd6
liuyihsuan <liuyihsuan1012@gmail.com>
2021-10-24 03:58:08 +0800

    project top right

--- src/views/ProductList.vue
+++ src/views/ProductList.vue
@@ -39,7 +39,49 @@
       </div>
     </nav>
   </div>
-
+  <!-- project top -->
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-8"></div>
+      <div class="col-lg-4 text-start">
+        <div class="product-introduce">
+          <div class="product-classification">
+            <a href="" class="">預購式專案</a> \
+            <a href="" class="">科技</a>
+          </div>
+          <a class="pc-title" href="">
+            <h4>
+              BLUETTI EB55 野獸級戶外行動電源站｜用「電」解放你的 outdoor 魂！
+            </h4>
+          </a>
+          <span class="proposer"> 提案人 <a href="" class="">蔚藍星球</a>
+            <!-- <a href=""> <i class="fab fa-facebook"></i></a>
+            <a href=""> <i class="fas fa-compass"></i></a>
+            <a href=""> <i class="fab fa-instagram"></i></a> -->
+          </span>
+          <p class="introduce">
+            海外儲能電源領導品牌 BLUETTI ，全台首發推出【 EB55
+            野獸級戶外行動電源站】一機搞定你的戶外用電需求，537Wh / 700W 具備 AC
+            市用插座及 DC 快充接孔
+          </p>
+          <div class="product-introduce-money">
+            <div class="circle-out">
+              <div class="circle-in"><span class="percentage">80%</span></div>
+            </div>
+            <div class="p-i-amount">
+              <p class="pre-order">預購金額</p>
+              <p class="order-amount">
+                NT$ <span class="amount">3,66,000</span>
+              </p>
+              <p class="target">
+                目標 NT$ <span class="target-amount">5,00,000</span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
   <!-- footer -->
   <div class="container footer mt-4">
     <div class="row footer-part">
@@ -82,4 +124,165 @@
 </template>
 <style lang="scss">
 
+
+.product-classification {
+  margin-bottom: 1rem;
+  font-size: 0.8rem;
+}
+
+.product-classification a {
+  color: #696969;
+  font-weight: bold;
+}
+
+.product-classification a:hover {
+  text-decoration: none;
+}
+.pc-title {
+  text-decoration: none;
+  color: #3f3f3f;
+}
+
+.pc-title h3 {
+  color: #000;
+  line-height: 2em;
+}
+
+.pc-title:hover {
+  text-decoration: none;
+}
+
+.proposer {
+  display: inline-block;
+  
+}
+
+.proposer a {
+  text-decoration: none;
+  padding: 0 0.2rem;
+  color: var(--font-color);
+  font-weight: bold;
+}
+
+.proposer a:not(:first-child) {
+  color: #696969;
+}
+
+.proposer a:hover {
+  text-decoration: none;
+  color: #000;
+}
+
+
+
+.introduce {
+  margin: 2rem 0;
+}
+
+/* 金額區 */
+
+.product-introduce-money {
+  display: flex;
+  padding-bottom: 2rem;
+  border-bottom: 1px solid #dcdcdc;
+}
+
+.circle-out {
+  width: 4rem;
+  height: 4rem;
+  background-color: #dcdcdc;
+  border-radius: 50%;
+  position: relative;
+}
+
+/* 需再加一個進度條 讓其可以轉動 */
+
+.circle-out:hover {
+  animation: percentage 1s;
+}
+
+@keyframes percentage {
+  0% {
+    background: #dcdcdc;
+  }
+  20% {
+    background: #0f1bcc;
+  }
+  60% {
+    background: #d87e2a;
+  }
+  80% {
+    background: #177a30;
+  }
+  100% {
+    background: #e50030;
+  }
+}
+
+.circle-in {
+  width: 3.5rem;
+  height: 3.5rem;
+  background-color: #fff;
+  border-radius: 50%;
+  position: absolute;
+  top: 0.25rem;
+  right: 0.25rem;
+}
+
+.percentage {
+  position: absolute;
+  top: 0.85rem;
+  right: 0.85rem;
+}
+
+.p-i-amount {
+  margin-left: 1rem;
+}
+
+.order-amount {
+  font-size: 1.5rem;
+  font-weight: bold;
+}
+/* product-middle */
+.product-middle {
+  margin-top: 1rem;
+  padding: 0 9rem;
+  display: flex;
+}
+.product-follow {
+  /* 暫時設定 */
+  /* background-color:#00008B; */
+  width: 660px;
+}
+
+.time {
+  margin-left: 2rem;
+  line-height: 2rem;
+  font-size: 0.5rem;
+  font-weight: bold;
+}
+
+.t-a-1 {
+  border: 1px solid #696969;
+  border-radius: 0.2rem;
+  padding: 0.2rem 0.6rem;
+  color: #696969;
+  text-decoration: none;
+  margin-right: 0.2rem;
+}
+.t-a-2 {
+  border: 1px solid #00008b;
+  border-radius: 0.2rem;
+  padding: 0.2rem 0.6rem;
+  color: #00008b;
+  text-decoration: none;
+  margin-right: 0.2rem;
+}
+.t-a-3 {
+  border: 1px solid #1e90ff;
+  border-radius: 0.2rem;
+  padding: 0.2rem 0.6rem;
+  color: #1e90ff;
+  text-decoration: none;
+}
 </style>
