cb9b659de5fd394e94f4a90df0e0fdde02e27d66
liuyihsuan <liuyihsuan1012@gmail.com>
2021-12-11 16:36:56 +0800

    手機右側出現空白區塊

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,6 @@
 <template>
   <!-- navbar -->
   <!-- 桌機板 -->
-  <!-- <div class="container"> -->
   <nav class="navbar navbar-expand navbar-light" style="background-color: #fff">
     <div class="container-fluid d-none d-md-block desktop-nav-part">
       <div class="navbar" id="navbarSupportedContent">
@@ -39,9 +38,8 @@
       </div>
     </div>
   </nav>
-  <!-- </div> -->
+  
   <!-- 手機版navbar -->
-  <!-- 探索按鈕移至下行 -->
   <nav class="navbar navbar-expand navbar-light phone-nav-top-part" style="background-color: #fff">
     <div class="container-fluid d-md-none">
       <a class="navbar-brand" href="#"
@@ -81,8 +79,38 @@
       </li>
     </ul>
   </nav>
+
   <!-- 挺好店專區 -->
-  <div class="covid">
+  <!-- 桌機板 -->
+  <div class="covid d-none d-md-block">
+    <div class="covid-img"></div>
+    <div class="covid-title">
+      <div class="covid-svg">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="35"
+          height="35"
+          fill="currentColor"
+          class="bi bi-shield-fill-plus"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M8 0c-.69 0-1.843.265-2.928.56-1.11.3-2.229.655-2.887.87a1.54 1.54 0 0 0-1.044 1.262c-.596 4.477.787 7.795 2.465 9.99a11.777 11.777 0 0 0 2.517 2.453c.386.273.744.482 1.048.625.28.132.581.24.829.24s.548-.108.829-.24a7.159 7.159 0 0 0 1.048-.625 11.775 11.775 0 0 0 2.517-2.453c1.678-2.195 3.061-5.513 2.465-9.99a1.541 1.541 0 0 0-1.044-1.263 62.467 62.467 0 0 0-2.887-.87C9.843.266 8.69 0 8 0zm-.5 5a.5.5 0 0 1 1 0v1.5H10a.5.5 0 0 1 0 1H8.5V9a.5.5 0 0 1-1 0V7.5H6a.5.5 0 0 1 0-1h1.5V5z"
+          />
+        </svg>
+      </div>
+      <h3>挺好店應援計畫</h3>
+      <span class="covid-span">
+        疫情讓許多你我的愛店面臨經營困難，嘖嘖規劃近期啟動「挺好店應援計畫」，提供<strong>實體空間／店家經營者</strong>和<strong>需要實體空間做展演的團隊</strong>應變疫情的上架方案。<a
+          href=""
+          >瞭解更多</a
+        >
+      </span>
+    </div>
+  </div>
+  <!-- 手機板 -->
+    <div class="covid-phone d-md-none">
     <div class="covid-img"></div>
     <div class="covid-title">
       <div class="covid-svg">
@@ -903,11 +931,54 @@
 }
 
 // 挺好店
+// 桌機版
 .covid {
   z-index: -1;
   position: relative;
   .covid-img {
     background-image: url("https://i.imgur.com/HnDfduT.png");
+    height: 25rem;
+    background-size: cover;
+  }
+  .covid-title {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    max-width: 26rem;
+    text-align: justify;
+    // 設定svg水平置中
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    .covid-svg {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 4rem;
+      height: 4rem;
+      background-color: #fff;
+      border-radius: 50%;
+      svg {
+        color: #0a6b0a;
+      }
+    }
+    h3 {
+      font-weight: bold;
+      padding-top: 1rem;
+    }
+    a {
+      color: var(--font-color);
+    }
+  }
+}
+// 手機版
+// 手機版本出現右側空白
+.covid-phone {
+  z-index: -1;
+  position: relative;
+  .covid-img {
+    background:url("https://i.imgur.com/HnDfduT.png") center center no-repeat;
     width: 100%;
     height: 25rem;
     background-size: cover;
@@ -944,6 +1015,7 @@
     }
   }
 }
+
 // 挺好店商品區
 .helpshop {
   margin-top: -2rem;
