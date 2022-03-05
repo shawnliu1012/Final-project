b94b411da28cd76573422277e12bcb1f497ce675
shawnliu <liuyihsuan1012@gmail.com>
2022-03-05 16:06:07 +0800

    modified shoppindcart navbar

--- src/views/Shoppingcart.vue
+++ src/views/Shoppingcart.vue
@@ -1,44 +1,87 @@
 <template>
-  <!-- navbar -->
-  <div class="container">
-    <nav
-      class="navbar navbar-expand-lg navbar-light"
-      style="background-color: #fff"
-    >
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#"
-          ><svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="28"
-            height="28"
-            fill="currentColor"
-            class="bi bi-arrow-up-circle-fill"
-            viewBox="0 0 16 16"
-          >
-            <path
-              d="M16 8A8 8 0 1 0 0 8a8 8 0 0 0 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"
-            />
-          </svg>
-          <img
-            src="https://i.imgur.com/Cqngd9w.png"
-            alt=""
-            width="50px"
-            height="40px"
-        /></a>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+    <!-- navbar -->
+  <!-- 桌機板 -->
+  <nav class="navbar navbar-expand navbar-light" style="background-color: #fff">
+    <div class="container-fluid d-none d-md-block desktop-nav-part">
+      <div class="navbar" id="navbarSupportedContent">
+        <div class="d-flex align-items-center">
+          <a class="navbar-brand" href="#"
+            ><svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="28"
+              height="28"
+              fill="currentColor"
+              class="bi bi-arrow-up-circle-fill"
+              viewBox="0 0 16 16"
+            >
+              <path
+                d="M16 8A8 8 0 1 0 0 8a8 8 0 0 0 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"
+              />
+            </svg>
+            <img
+              src="https://i.imgur.com/Cqngd9w.png"
+              alt=""
+              width="50px"
+              height="40px"
+          /></a>
+
+          <ul class="navbar-nav">
             <li class="nav-item">
               <a class="nav-link" aria-current="page" href="#">探索</a>
             </li>
           </ul>
-          <div class="d-flex navbar-btn">
-            <button class="btn btn-light" type="submit">登入</button>
-            <button class="btn btn-light" type="submit">註冊</button>
-          </div>
+        </div>
+        <div class="navbar-btn">
+          <button class="btn btn-light" type="submit">登入</button>
+          <button class="btn btn-light" type="submit">註冊</button>
         </div>
       </div>
-    </nav>
-  </div>
+    </div>
+  </nav>
+
+  <!-- 手機版navbar -->
+  <nav
+    class="navbar navbar-expand navbar-light phone-nav-top-part"
+    style="background-color: #fff"
+  >
+    <div class="container-fluid d-md-none">
+      <a class="navbar-brand" href="#"
+        ><svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="28"
+          height="28"
+          fill="currentColor"
+          class="bi bi-arrow-up-circle-fill"
+          viewBox="0 0 16 16"
+        >
+          <path
+            d="M16 8A8 8 0 1 0 0 8a8 8 0 0 0 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"
+          />
+        </svg>
+        <img
+          src="https://i.imgur.com/Cqngd9w.png"
+          alt=""
+          width="50px"
+          height="40px"
+      /></a>
+      <div class="navbar" id="navbarSupportedContent">
+        <div class="navbar-btn">
+          <button class="btn btn-light" type="submit">登入</button>
+          <button class="btn btn-light" type="submit">註冊</button>
+        </div>
+      </div>
+    </div>
+  </nav>
+  <nav
+    class="navbar navbar-expand navbar-light d-md-none phone-nav-bottom-part"
+    style="background-color: #fff"
+  >
+    <ul class="navbar-nav mx-auto">
+      <li class="nav-item">
+        <a class="nav-link" aria-current="page" href="#">探索</a>
+      </li>
+    </ul>
+  </nav>
   <!-- product container -->
   <div class="container-line"></div>
   <div class="container shoppingcart">
