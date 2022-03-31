1a7c9421dd1e4ce0e0ba43a63a332d180c06ab6e
shawnliu <liuyihsuan1012@gmail.com>
2022-03-31 20:34:28 +0800

    delete vue3-circle-progress

--- src/main.js
+++ src/main.js
@@ -4,6 +4,5 @@ import App from './App.vue'
 import router from './router'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
-import CircleProgress from 'vue3-circle-progress'
 
-createApp(App).use(router).use(VueAxios, axios).use(CircleProgress).mount('#app')
+createApp(App).use(router).use(VueAxios, axios).mount('#app')
