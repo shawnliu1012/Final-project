ec9d13e68ceb05ab197880783e20506418c62b49
shawnliu <liuyihsuan1012@gmail.com>
2022-04-13 19:26:51 +0800

    modified app.vue

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
     <router-link to="/about">About</router-link> |
     <router-link to="/explore">Explore</router-link> |
     <router-link to="/products">Products</router-link> |
-    <router-link to="/shoppingcart">Shoppingcart</router-link> -->
+    <router-link to="/shoppingcart">Shoppingcart</router-link> 
     
   </div>
   <router-view/>
